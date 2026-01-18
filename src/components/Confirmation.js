7b75d5be87afb9d490af2a1fe3dc052f2ddcaf5a
Lerxuanyikate <kateler6517@gmail.com>
2026-01-18 23:37:15 +0800

    r

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -9,7 +9,7 @@ export default function Confirmation() {
       {state && (
         <>
       <p>{state.name}You're now registered for Red30 Tech.</p>
-      <p>We've sent more details to <strong>{state.email}</strong></p>
+      <p>We've sent more details to <strong>{state.email}</strong></p> 
       </>
       )}
     </div>
