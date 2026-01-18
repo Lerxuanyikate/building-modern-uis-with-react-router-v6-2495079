d6599cee66fed6173ecc9fc194e95f714d7a1326
Lerxuanyikate <kateler6517@gmail.com>
2026-01-18 23:38:23 +0800

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,30 @@
-import { Routes, Route } from "react-router-dom";
-
+import {Routes, Route} from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Categories from "./Categories";
 import Category from "./Category";
 import Header from "./Header";
+import Session from "./Session";
+import Register from "./Register";
+import Confirmation from "./Confirmation";
 
 function App() {
   return (
     <div className="app">
       <Header />
-
       <Routes>
-        <Route path="/" element={<Home title="Welcome to Red30 Tech" />} />
-        <Route path="about" element={<About />} />
-        <Route path="categories" element={<Categories />}>
-          <Route path=":catId" element={<Category />} />
+        <Route path="/" element={<Home title="Welcome to Red30 tech"/>} />
+        <Route path="About" element={<About/>} />
+        <Route path="categories" element={<Categories/>} >
+          <Route path=" :catId" element={<Category/>}>
+            <Route path=":sessionId" element={<Session/>} />
+          </Route>
+          <Route index element={<h3> Select a category from above</h3>} />
         </Route>
-        <Route
-          path="*"
-          element={<h1 className="not-found">Page Not Found</h1>}
-        />
+        <Route path="register" element={<Register/>} />
+        <Route path="confirmed" element={<Confirmation/>} />
+        <Route path="*" 
+        element={<h1 className="not-found">404: Page Not Found</h1>} />
       </Routes>
 
       <footer className="container">
